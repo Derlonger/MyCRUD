28ea588971e5d28ff921d9420bb7c3a45af108c1
Derlonger <add.email@mail.ru>
2024-03-09 08:51:40 +0300

    Create Connecting with DataBase

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,1 +1,21 @@
 package config
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func Connect() {
+	dsn := "host=localhost user=admin password=admin dbname=users port=9920 sslmode=disable"
+	d, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
