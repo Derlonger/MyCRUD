e35472ae073998ac9c37d21942e378f89d37539e
Derlonger <add.email@mail.ru>
2024-03-09 09:18:19 +0300

    Add User Query

--- pkg/models/user.go
+++ pkg/models/user.go
@@ -1,8 +1,49 @@
 package models
 
+import (
+	"gorm.io/gorm"
+	"log"
+	"modulepath/pkg/config"
+)
+
+var db *gorm.DB
+
 type User struct {
 	Id      uint64 `gorm:"primaryKey" json:"id"`
 	Name    string `json:"name"`
 	Email   string `json:"email"`
 	Address string `json:"address"`
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	err := db.AutoMigrate(&User{})
+	if err != nil {
+		log.Println("Ошибка при миграции таблицы User:", err)
+		return
+	}
+}
+
+func AddUser(us *User) *User {
+	db.Create(&us)
+	return us
+}
+
+func GetAllUsers() []User {
+	var Users []User
+	db.Find(&Users)
+	return Users
+}
+
+func GetUserById(id uint64) (*User, *gorm.DB) {
+	var user *User
+	db.Find(&user, id)
+	return user, db
+}
+
+func DeleteUserById(id uint64) *User {
+	var user *User
+	db.Delete(&user, id)
+	return user
 }
