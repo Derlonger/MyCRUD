272cb3408babc975ce4f936fd15d2234619e1637
Derlonger <add.email@mail.ru>
2024-03-09 08:56:41 +0300

    Create struct User

--- pkg/models/user.go
+++ pkg/models/user.go
@@ -1,1 +1,8 @@
 package models
+
+type User struct {
+	Id      uint64 `gorm:"primaryKey" json:"id"`
+	Name    string `json:"name"`
+	Email   string `json:"email"`
+	Address string `json:"address"`
+}
