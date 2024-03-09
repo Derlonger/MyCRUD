f5d91da693ade6fb65c30e3e550099c0b091b5a7
Derlonger <add.email@mail.ru>
2024-03-09 08:21:40 +0300

    Create routers

--- pkg/routes/user_routes.go
+++ pkg/routes/user_routes.go
@@ -1,1 +1,11 @@
 package routes
+
+import "github.com/gorilla/mux"
+
+var RegisterUserRoutes = func(router *mux.Router) {
+	router.HandleFunc("/users", controllers.GetUser).Methods("GET")
+	router.HandleFunc("/users", controllers.AddUser).Methods("POST")
+	router.HandleFunc("/users/{userId}", controllers.GetUserById).Methods("GET")
+	router.HandleFunc("/users{userId}", controllers.UpdateUser).Methods("PUT")
+	router.HandleFunc("/users{userId}", controllers.DeleteUser).Methods("DELETE")
+}
