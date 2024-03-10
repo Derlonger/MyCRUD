4f593a7b493c5558c7a353432a964cf9bd4055c0
Derlonger <add.email@mail.ru>
2024-03-10 10:01:06 +0300

    fix function "GetUser" and "AddUser"

--- pkg/controllers/user-controller.go
+++ pkg/controllers/user-controller.go
@@ -1,27 +1,78 @@
 package controllers
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 	"strconv"
+	"text/template"
 
 	"github.com/gorilla/mux"
 	"modulepath/pkg/models"
 	"modulepath/pkg/utils"
 )
 
-// GetUser возвращает всех пользователей в формате JSON
-func GetUser(writer http.ResponseWriter, _ *http.Request) {
+// GetUser возвращает список всех пользователей
+func GetUser(writer http.ResponseWriter, request *http.Request) {
 	allUsers := models.GetAllUsers()
-	utils.RespondWithJSON(writer, http.StatusOK, allUsers)
+	log.Printf("Получены следующие пользователи: %+v\n", allUsers) // Добавлено логирование
+	// Проверяем заголовок Accept на предмет запроса JSON
+	if request.Header.Get("Accept") == "application/json" {
+		// Если запрос на JSON, возвращаем JSON ответ
+		log.Println("Запрошен список всех пользователей (JSON).")
+		utils.RespondWithJSON(writer, http.StatusOK, allUsers)
+		return
+	}
+
+	// Если запрос на HTML, возвращаем HTML страницу
+	log.Println("Запрошен список всех пользователей (HTML).")
+	tmpl, err := template.ParseFiles("templates/users.html")
+	if err != nil {
+		http.Error(writer, "Ошибка при загрузке шаблона", http.StatusInternalServerError)
+		log.Println("Ошибка при загрузке шаблона:", err)
+		return
+	}
+
+	writer.Header().Set("Content-Type", "text/html; charset=utf-8")
+	if err := tmpl.Execute(writer, allUsers); err != nil {
+		http.Error(writer, "Ошибка при отображении страницы", http.StatusInternalServerError)
+		log.Println("Ошибка при заполнении шаблона:", err)
+		return
+	}
 }
 
 // AddUser добавляет нового пользователя и возвращает его данные в формате JSON
 func AddUser(writer http.ResponseWriter, request *http.Request) {
-	addUser := &models.User{}
-	utils.ParseBody(request, addUser)
-	user := models.AddUser(addUser)
-	utils.RespondWithJSON(writer, http.StatusOK, user)
+	// Получаем данные из формы
+	name := request.FormValue("name")
+	email := request.FormValue("email")
+	address := request.FormValue("address")
+
+	// Создаем нового пользователя
+	newUser := &models.User{
+		Name:    name,
+		Email:   email,
+		Address: address,
+	}
+
+	// Добавляем пользователя в базу данных
+	user := models.AddUser(newUser)
+
+	// Отправляем ответ клиенту
+	res, err := json.Marshal(user)
+	if err != nil {
+		http.Error(writer, "Ошибка при обработке данных пользователя", http.StatusInternalServerError)
+		log.Println("Ошибка при обработке данных пользователя:", err)
+		return
+	}
+
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusOK)
+	_, err = writer.Write(res)
+	if err != nil {
+		log.Println("Ошибка при записи ответа клиенту:", err)
+		return
+	}
 }
 
 // GetUserById возвращает данные пользователя по указанному ID в формате JSON
@@ -33,6 +84,7 @@ func GetUserById(writer http.ResponseWriter, request *http.Request) {
 		log.Println("UserId not integer")
 	}
 	userDetails, _ := models.GetUserById(id)
+	log.Printf("Запрошены данные пользователя с ID %d\n", id)
 	utils.RespondWithJSON(writer, http.StatusOK, userDetails)
 }
 
@@ -57,6 +109,7 @@ func UpdateUser(writer http.ResponseWriter, request *http.Request) {
 		userDetails.Address = updateUser.Address
 	}
 	db.Save(&userDetails)
+	log.Printf("Обновлены данные пользователя с ID %d\n", id)
 	utils.RespondWithJSON(writer, http.StatusOK, userDetails)
 }
 
@@ -69,5 +122,6 @@ func DeleteUser(writer http.ResponseWriter, request *http.Request) {
 		log.Println("Userid not integer")
 	}
 	user := models.DeleteUserById(id)
+	log.Printf("Удален пользователь с ID %d\n", id)
 	utils.RespondWithJSON(writer, http.StatusOK, user)
 }
