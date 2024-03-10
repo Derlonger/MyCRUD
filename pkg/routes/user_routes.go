57f748104bd0a26976ed3380fde38a466e8a992a
Derlonger <add.email@mail.ru>
2024-03-10 10:00:30 +0300

    fix add "/"

--- pkg/routes/user_routes.go
+++ pkg/routes/user_routes.go
@@ -9,6 +9,6 @@ var RegisterUserRoutes = func(router *mux.Router) {
 	router.HandleFunc("/users", controllers.GetUser).Methods("GET")
 	router.HandleFunc("/users", controllers.AddUser).Methods("POST")
 	router.HandleFunc("/users/{userId}", controllers.GetUserById).Methods("GET")
-	router.HandleFunc("/users{userId}", controllers.UpdateUser).Methods("PUT")
-	router.HandleFunc("/users{userId}", controllers.DeleteUser).Methods("DELETE")
+	router.HandleFunc("/users/{userId}", controllers.UpdateUser).Methods("PUT")
+	router.HandleFunc("/users/{userId}", controllers.DeleteUser).Methods("DELETE")
 }
