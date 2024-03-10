ccc39a578a2f83d2c5b9e2fa099479cf889ae0d5
Derlonger <add.email@mail.ru>
2024-03-10 08:43:01 +0300

    create func RespondWithJSON

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -13,3 +13,20 @@ func ParseBody(r *http.Request, x interface{}) {
 		}
 	}
 }
+
+// RespondWithJSON записывает данные в формате JSON в тело ответа и отправляет его клиенту
+func RespondWithJSON(w http.ResponseWriter, statusCode int, data interface{}) {
+	response, err := json.Marshal(data)
+	if err != nil {
+		http.Error(w, "Ошибка при обработке данных", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	_, err = w.Write(response)
+	if err != nil {
+		// В случае ошибки при записи ответа клиенту, можно просто вернуться, поскольку уже был отправлен ответ с ошибкой
+		return
+	}
+}
