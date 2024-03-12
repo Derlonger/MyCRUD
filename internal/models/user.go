efecc431d7e50d83df00785337593a27ef048b58
Derlonger <add.email@mail.ru>
2024-03-12 08:36:06 +0300

    clear modules

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,52 +1,8 @@
 package models
 
-import (
-	"gorm.io/gorm"
-	"log"
-	"modulepath/internal/config"
-)
-
-var db *gorm.DB
-
 type User struct {
 	ID      uint64 `gorm:"primaryKey" json:"id"`
 	Name    string `json:"name"`
 	Email   string `json:"email"`
 	Address string `json:"address"`
-}
-
-func init() {
-	config.Connect()
-	db = config.GetDB()
-	err := db.AutoMigrate(&User{})
-	if err != nil {
-		log.Println("Ошибка при миграции таблицы User:", err)
-		return
-	}
-}
-
-func AddUser(us *User) *User {
-	db.Create(&us)
-	return us
-}
-
-func GetAllUsers() []User {
-	var Users []User
-	result := db.Find(&Users)
-	if result.Error != nil {
-		log.Println("Ошибка при получении списка пользователей:", result.Error)
-	}
-	return Users
-}
-
-func GetUserById(id uint64) (*User, *gorm.DB) {
-	var user *User
-	db.Find(&user, id)
-	return user, db
-}
-
-func DeleteUserById(id uint64) *User {
-	var user *User
-	db.Delete(&user, id)
-	return user
 }
